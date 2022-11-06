68819ba4c9478a9689e23e20eb2896b8aa60ea08
Khai Tran <khaiqtran13@gmail.com>
2022-11-06 16:21:10 -0500

    basic template for tweet component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import "./App.css";
+import TweetComponent from "./TweetComponent";
 
 function App() {
   return (
@@ -8,7 +8,9 @@ function App() {
         <div className="h-full justify-center flex bg-pink-300">Sidebar</div>
         <div className="justify-between flex flex-col">
           Mid Section Top
-          <div className="border-4">Tweet Component - Khai</div>
+          <div className="p-8">
+            <TweetComponent />
+          </div>
           <div className="border-4">Infinite Scroll Component</div>
           Mid Section Bottom
         </div>
