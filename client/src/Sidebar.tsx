11ebdd2e944dbff6eea901c17a88d8ddcc57b999
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2022-11-06 22:46:22 -0500

    Added filter functionality to sidebar
    Sidebar now is capable of switching which content shows (ideas or challenges). Currently console.logs, future implementation should use the conditionals to filter content

--- client/src/Sidebar.tsx
+++ client/src/Sidebar.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import { slide as Menu } from 'react-burger-menu';
 import './Sidebar.css';
 
+// Need to change console logs to switching the type of elements shown
+function changeType(type: string) {
+  if (type == 'idea') {
+    console.log('idea')
+  }
+  else {
+    console.log('challenge')
+  }
+};
 
 export default props => {
 
@@ -10,10 +19,10 @@ export default props => {
         
         {/* Replace href with an onClick that handles which menu item was clicked, and then use that to either display ideas or challenges */}
         
-        <a className="menu-item" href="/"> 
+        <a className="menu-item" onClick={() => changeType('idea')}> 
           Ideas
         </a>
-        <a className="menu-item" href="/">
+        <a className="menu-item" onClick={() => changeType('challenge')}>
           Challenges
         </a>
       </Menu>
