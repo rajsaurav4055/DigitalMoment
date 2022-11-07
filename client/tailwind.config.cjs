4671db82e3de3218deab41d3577c1d7686ae64fe
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2022-11-06 22:09:47 -0500

    Infinite Scroll Container

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+      animation : {
+        'spin-slow-30': 'spin 30s linear infinite',
+        'spin-slow-25': 'spin 25s linear infinite',
+        'spin-slow-10': 'spin 10s linear infinite',
+        'marquee-infinite' : 'marquee 25s linear infinite',
+      },
+    },
   },
   plugins: [],
-};
+}
