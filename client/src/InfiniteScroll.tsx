0702568b13a9edca88182a743306a8ffc62c7fc5
Khai Tran <khaiqtran13@gmail.com>
2022-11-06 23:24:14 -0500

    all you ava

--- client/src/InfiniteScroll.tsx
+++ client/src/InfiniteScroll.tsx
@@ -1,70 +1,72 @@
 import React from "react";
 import PP from "./assets/profile_pic.png";
+import SubSection from "./SubSection";
 
 type Props = {};
 
 const InfiniteScroll = (props: Props) => {
   return (
-      <div className="shadow-md border-[10px] border-gray-50 rounded-xl p-4 bg-[#FCFCFC] text-left max-h-72 overflow-auto">
-        <div>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #DigitalMoment
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #CodeToGive
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">
-            #MorganStanley
-          </p>
-          <p className="px-2 py-1 text-emerald-600 hover:underline">... more</p>
+    <div className="shadow-md border-[10px] border-gray-50 rounded-xl p-4 bg-[#FCFCFC] text-left max-h-72 overflow-auto">
+      <div>
+        <SubSection name="Chetan Mangay" />
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #DigitalMoment
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #CodeToGive
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">
+          #MorganStanley
+        </p>
+        <p className="px-2 py-1 text-emerald-600 hover:underline">... more</p>
       </div>
     </div>
   );
